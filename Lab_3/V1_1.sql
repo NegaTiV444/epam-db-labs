c091f3d92e65a0f10e0a9b8d6cc555cade207eba
Vladislav Dubovski <444negativ@gmail.com>
2020-10-31 22:19:01 +0300

    Add-lab-3

---
+++ Lab_3/V1_1.sql
@@ -0,0 +1,90 @@
+USE AdventureWorks2012;
+GO
+
+--------------------------- SUBTASK a ---------------------------
+
+ALTER TABLE dbo.Person
+ADD FullName nvarchar(100);
+GO
+
+--------------------------- SUBTASK b ---------------------------
+
+DECLARE @Person TABLE (
+	BusinessEntityID INT,
+	PersonType nchar(2),
+	NameStyle NameStyle NULL,
+	Title nvarchar(8),
+	FirstName Name,
+	MiddleName Name,
+	LastName Name,
+	Suffix nvarchar(5),
+	EmailPromotion INT,
+	ModifiedDate datetime,
+	ID bigint,
+	FullName nvarchar(100)
+);
+
+INSERT INTO @Person (
+	BusinessEntityID,
+	PersonType,
+	NameStyle,
+	Title,
+	FirstName,
+	MiddleName,
+	LastName,
+	Suffix,
+	EmailPromotion,
+	ModifiedDate,
+	ID,
+	FullName
+)
+SELECT
+	person.BusinessEntityID,
+	person.PersonType,
+	person.NameStyle,
+	CASE employee.Gender
+		WHEN 'M' THEN 'Mr.'
+		WHEN 'F' THEN 'Ms.'
+		ELSE NULL
+	END as Title,
+	person.FirstName,
+	person.MiddleName,
+	person.LastName,
+	person.Suffix,
+	person.EmailPromotion,
+	person.ModifiedDate,
+	person.ID,
+	person.FullName
+FROM dbo.Person person
+LEFT JOIN HumanResources.Employee employee ON employee.BusinessEntityID = person.BusinessEntityID;
+
+--------------------------- SUBTASK c ---------------------------
+
+MERGE INTO dbo.Person person
+USING @Person person2 ON person.ID = person2.ID
+WHEN MATCHED THEN
+   UPDATE SET person.FullName = CONCAT(person2.Title, ' ', person2.FirstName, ' ', person2.LastName);
+
+--------------------------- SUBTASK d ---------------------------
+
+DELETE FROM dbo.Person WHERE LEN(FullName) > 20;
+
+--------------------------- SUBTASK e ---------------------------
+
+DECLARE @Query nvarchar(512) = ' ALTER TABLE dbo.Person DROP CONSTRAINT '
+SELECT @Query = @Query + name + ', '
+FROM sys.objects
+WHERE type IN ('C', 'D') AND parent_object_id = (
+SELECT object_id
+FROM sys.objects
+WHERE schema_Name(schema_id) = 'dbo' AND name = 'Person')
+SET @Query = RTRIM(SUBSTRING(@Query, 0, LEN(@Query)))
+
+execute (@Query)
+
+ALTER TABLE dbo.Person
+DROP COLUMN ID;
+
+--------------------------- SUBTASK f ---------------------------
+
+DROP TABLE dbo.Person;
